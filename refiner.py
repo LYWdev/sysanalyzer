9385696b66b36aba5d3a17ee8aa35b1ce9d856ed
ywl <ywl@ywls-MacBook-Pro.local>
2024-09-20 22:37:23 +0900

    edit refiner.py chunk단위로 로그파일 처리하는 기능추가

--- refiner.py
+++ refiner.py
@@ -1,15 +1,21 @@
-# 파일 경로 설정
-input_file = "mongo_docker.log"
-output_file = "refined_mongo_docker.log"
+import os
 
-# 파일 읽기 및 처리
-with open(input_file, "r") as file:
-    lines = file.readlines()
+input_file = "strace_gvisor"
+output_file = "strace_gvisor_refined"
+chunk_size = 1024 * 1024 * 50000  #50000 *1MB 청크 단위
 
-# 특정 문자열을 포함하는 행을 제외하고 저장
-with open(output_file, "w") as file:
-    for line in lines:
-        if "<unfinished ...>" not in line and "resumed" not in line:
-            file.write(line)
+# 청크 단위로 파일을 처리
+with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+    while True:
+        # 청크 단위로 읽어들임
+        lines = f_in.readlines(chunk_size)
+        if not lines:
+            break
+
+        # 조건에 맞는 라인만 출력
+        for line in lines:
+            if "<unfinished ...>" not in line and "resumed" not in line:
+                f_out.write(line)
 
 print(f"Filtered log has been saved to {output_file}")
+
